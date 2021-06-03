8e4f78825d43d6bf4ed724c79bc9fd28775b042c
Lam Tu <lam.tu@xero.com>
2021-06-03 17:19:52 +1000

    Puzzle???

---
+++ ConsoleApp2/Extension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public static class Extension
+    {
+        public static Task ParallelForEachAsync<T>(this IEnumerable<T> source, int partitions, Func<T, Task> body)
+        {
+            async Task AwaitPartition(IEnumerator<T> partition)
+            {
+                using (partition)
+                {
+                    while (partition.MoveNext())
+                    { await body(partition.Current); }
+                }
+            }
+
+            return Task.WhenAll(
+                Partitioner
+                    .Create(source)
+                    .GetPartitions(partitions)
+                    .AsParallel()
+                    .Select(p => AwaitPartition(p)));
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 100).ToList();
+            numbers.ParallelForEachAsync(10, async number =>
+            {
+                var writtenNumbers = new List<int>();
+                Console.WriteLine($"Writing number {number}");
+                Thread.Sleep(1);
+                Console.WriteLine($"Finished writing number {number}");
+            });
+        }
+    }
+}
